208a159f1715f2a87c9d4723e26e24f6240f5714
Sercambet <sergiomcamargo@hotmail.com>
2026-03-25 20:10:26 -0500

    se añade convertidor de mm a cms

---
+++ ejercicio.js
@@ -0,0 +1,13 @@
+let nombre=prompt('dime tu nombre')
+console.log (typeof nombre)
+
+let valor= Number (prompt('dime valor de compra'));
+console.log( typeof valor);
+console.log(`Hola` , nombre,  `tu valor de compra es`, valor + `tu valor a pagar con descuento es`+  valor*0.20);
+
+
+if(valor=>100){
+     valor*descuento/100*0.10
+}
+   
+    
---
+++ script.js
@@ -0,0 +1,5 @@
+let medida=(prompt ('digita tu medida en milímetros') );
+console.log('ok tu medida es' + medida)
+alert ('confirmo que tu medida en milimetros es: ' + medida +' milímetros')
+alert ('entonces tu medida en centimetros es: ' + medida/10 +' centímetros')
+
