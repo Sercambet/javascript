7888e9df6739f3a0193d47bf9c61b4e29137abb0
Sercambet <sergiomcamargo@hotmail.com>
2026-03-25 20:16:19 -0500

    se añade calculadora perruna

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// convertidor a edad canina
+
+let edadHumano=(prompt('digita tu edad humana'));
+alert ('tu edad perruna es '  + edadHumano*7);
+
+
+
+
+
 let medida=(prompt ('digita tu medida en milímetros') );
 console.log('ok tu medida es' + medida)
 alert ('confirmo que tu medida en milimetros es: ' + medida +' milímetros')
