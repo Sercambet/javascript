45a27f7bd066547a8d2dc210cfc0eaebf8ec957c
Sercambet <sergiomcamargo@hotmail.com>
2026-03-25 20:33:51 -0500

    calculadora de pesos a dolares y viceversa

--- script.js
+++ script.js
@@ -3,9 +3,20 @@
 let edadHumano=(prompt('digita tu edad humana'));
 alert ('tu edad perruna es '  + edadHumano*7);
 
+// convetidor pesos a dolares
+
+let COP=(prompt('digita tu valor en PESOS (COP)'));
+alert(COP + ' COP equivalen a $'  + (COP/3693).toFixed(2) + 'USD');
+
+let USD=(prompt('digita tu valor en dolares'));
+alert(USD + 'USD equivalen a $' + (USD*3963).toLocaleString('es-CO') + ' COP ');
+
+// 
+
+//calculadora de IMC
 
 
-
+// convertidor de milimetros a centimetros
 
 let medida=(prompt ('digita tu medida en milímetros') );
 console.log('ok tu medida es' + medida)
