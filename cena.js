633ec97f1700ba74e117da5c155e0bd3e7045e36
Sercambet <sergiomcamargo@hotmail.com>
2026-03-26 21:17:32 -0500

    listo la cuenta al detalle

--- cena.js
+++ cena.js
@@ -2,23 +2,26 @@
 let valorcena=Number(prompt('Cuanto costó la cena?')); //500.0000
 let comensales=Number(prompt('Cuantos comensales hubo?'));
 let consumomayor=String(prompt('Quien comió más?'));
+let consumomenor=String(prompt('Quien comió menos?'))
 
-let cxpersona=valorcena/comensales; //500.000 / 4
-console.log(cxpersona) //$125.000
+let cxpersona=valorcena/comensales; 
+console.log(cxpersona) 
 
-let juanpaga=cxpersona*2 // $125.000 * 2
-console.log(consumomayor + 'tiene que pagar' + juanpaga) // $250.000
+let juanpaga=cxpersona*2 
+console.log(consumomayor + 'tiene que pagar' + juanpaga) 
 
+let valorpedro=cxpersona/2
+console.log(consumomenor + 'solo tiene que pagar' + valorpedro + 'porque comió poquito')
 
-let comensalessinjuan=Number=comensales-1;
+let comensalessinjuan=Number=comensales-2;
 console.log('se divide el resto entre ' + comensalessinjuan + ' personas ')
 
-let valorcuentasinjuan=valorcena-juanpaga
+let valorcuentasinjuan=valorcena-juanpaga-valorpedro
 console.log('el saldo a pagar de los demás es' + valorcuentasinjuan)
 
 let valorpagarlos3=valorcuentasinjuan/comensalessinjuan
-console.log('el valor a pagar por cabeza es de ' + valorpagarlos3)
+console.log('el valor a pagar por cabeza de los restantes es' + valorpagarlos3)
 
 let sumadetodos=juanpaga+valorpagarlos3*comensalessinjuan
-console.log(consumomayor + ' paga' + juanpaga +' mientras que los demás reunen en total '+  valorpagarlos3 + ' para saldar la cuenta total de ' + sumadetodos )
+console.log(consumomayor + ' paga ' + juanpaga + ' por tragón ' + consumomenor + ' que consumió poquito solo paga ' + valorpedro +' los demás deben pagar ' + valorcuentasinjuan + ' esto consolidando la cuenta total de ' + (valorpedro+valorcuentasinjuan+juanpaga)  )
 
